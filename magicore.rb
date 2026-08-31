7f57e586d3e4db58c86bbf837a0bad730ba55af2
doanmihh153 <doanmihh1533@gmail.com>
2026-08-31 15:28:39 +0700

    magicore 1.0.0-rc.2

--- magicore.rb
+++ magicore.rb
@@ -1,16 +1,16 @@
 class Magicore < Formula
   desc "Universal Package Manager with Zero-Trust Supply Chain"
   homepage "https://github.com/mingd-153/MagiCore"
-  version "1.0.0-rc.1"
-  
+  version "1.0.0-rc.2"
+
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/mingd-153/MagiCore/releases/download/v1.0.0-rc.1/magicore-macOS-ARM64.tar.gz"
-    sha256 "9b24ae6eb0379c00e64749b5584d6adf203217f1d045861fdea878d129921e4e"
+    url "https://github.com/mingd-153/MagiCore/releases/download/v1.0.0-rc.2/magicore-macOS-ARM64.tar.gz"
+    sha256 "8382022aec645f9a0355cdec411d1a05741ee4f3bddb56a54ee832fa8b9e45f3"
   elsif OS.linux?
-    url "https://github.com/mingd-153/MagiCore/releases/download/v1.0.0-rc.1/magicore-Linux-X64.tar.gz"
-    sha256 "f3cdd2e4351a31105391cb438d27a5f18827f73313d945c0dbecc63df567704a"
+    url "https://github.com/mingd-153/MagiCore/releases/download/v1.0.0-rc.2/magicore-Linux-X64.tar.gz"
+    sha256 "aefe16567841b2f0e45eb91164e1e7e7f3101707b325d47091eceb9c0079bbc8"
   else
-    odie "Unsupported platform. Only macOS ARM64 and Linux x86_64 available in RC.1"
+    odie "Unsupported platform. Only macOS ARM64 and Linux x86_64 available in RC.2"
   end
 
   def install
